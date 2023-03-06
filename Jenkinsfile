22deda113eff718e1616a59bbab88978007c353c
javiercinhoo <javiercinhoo@hotmail.com>
2023-03-06 16:14:47 -0500

    Subiendo el proyecto Local

---
+++ Jenkinsfile
@@ -0,0 +1,25 @@
+pipeline{
+	agent { label 'master'}
+		stages{
+		
+			stage ('test'){
+				steps{
+					//dir("build_java"){
+						Sh "gradle test -DcucumberOptions"
+					//}
+				}
+			}
+
+			stage ('Build application'){
+				steps{
+						echo "mvn clean install -Dmaven.test.skip=true"
+				}
+			}
+			
+			stage ('Create docker image'){
+				steps{
+						echo "creando docker"
+				}
+			}
+		}
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const report = require("multiple-cucumber-html-reporter");
+
+report.generate({
+  jsonDir: "reports",
+  reportPath: "reports",
+
+  openReportInBrowser: true,
+  displayReportTime: true,
+  displayDuration: true,
+  saveCollectedJSON :false,
+  reportName:" Report Test Node",
+  pageFooter:"<center>By Javier Duarte</center>", 
+  hideMetadata: true,
+  
+  customData: {
+    title: "Run info",
+    data: [
+      { label: "Project", value: "Test Node" },
+      { label: "Release", value: "1.0" },
+    ],
+  },
+});
